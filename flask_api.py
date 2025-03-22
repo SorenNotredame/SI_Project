f75e892b3c37c6fe5d2c35bfa0d3d8d0d1190dfb
SorenNotredame <r0982640@student.thomasmore.be>
2025-03-22 20:59:48 +0100

    classe broken?

--- flask_api.py
+++ flask_api.py
@@ -4,6 +4,7 @@ from flask_cors import *
 import Data_acquisition
 from threading import Thread
 import peak_predictor
+from zigbee2mqtt_class import ZigbeeController
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +14,7 @@ values = {'kwartiervermorgen_gewenst': '3500', 'a_manueel': False, 'b_manueel': 
 
 def flask_thread():
     app.run(host="0.0.0.0")
+    
 
 def data_thread():
     Data_acquisition.main()
@@ -23,9 +25,74 @@ def peak_thread():
         peak.MAX_PEAK = int(values["kwartiervermorgen_gewenst"])
         peak.main()
         values['hoogste_kwartiervermogen'] = f"{peak.max_average_power:.2f}"
+        
+        if 'zigbee_controller' not in globals():
+            mqtt_broker_address = "localhost"
+            zigbee_controller = ZigbeeController(mqtt_broker_address)
+            device_id_a = "Type-A"
+            zigbee_controller.subscribe_to_device(device_id_a)
+            device_id_b = "Type-B"
+            zigbee_controller.subscribe_to_device(device_id_b)
+            device_id_c = "Type-C"
+            zigbee_controller.subscribe_to_device(device_id_c)
         values['a_turned_off'] = peak.ATurnOff
+        
+        if not values['a_manueel']:
+            
+            # Check if the device should be turned OFF
+            if peak.ATurnOff:
+                #!!!!!!dit staat gecomment omdat get_device_state geen waarde geeft als dit werk mag dit worden uncomment en geindent
+                """ if zigbee_controller.get_device_state(device_id_a) == "ON": """
+                zigbee_controller.turn_off_device(device_id_a)
+                print(f"{device_id_a} turned OFF due to peak.ATurnOff")
+
+            # Check if the device should be turned back ON
+            else:  # peak.ATurnOff is False
+                #!!!!!!!dit staat gecomment omdat get_device_state geen waarde geeft als dit werk mag dit worden uncomment en geindent
+                """ if zigbee_controller.get_device_state(device_id_a) == "OFF": """
+                zigbee_controller.turn_on_device(device_id_a)
+                print(f"{device_id_a} turned ON as peak.ATurnOff is now False")
+        #!!!!!!!dit staat gecomment omdat get_device_state geen waarde geeft als dit werk mag dit worden uncomment en geindent
+        """ else: 
+            if zigbee_controller.get_device_state(device_id_a) == "OFF":
+                    zigbee_controller.turn_on_device(device_id_a) """
+
         values['b_turned_off'] = peak.BTurnOff
+
+        """ if not values['b_manueel']:
+            # Check if the device should be turned OFF
+            if peak.BTurnOff:
+                if zigbee_controller.get_device_state(device_id_b) == "ON":
+                    zigbee_controller.turn_off_device(device_id_b)
+                    print(f"{device_id_b} turned OFF due to peak.BTurnOff")
+
+            # Check if the device should be turned back ON
+            else:  # peak.BTurnOff is False
+                if zigbee_controller.get_device_state(device_id_b) == "OFF":
+                    zigbee_controller.turn_on_device(device_id_b)
+                    print(f"{device_id_b} turned ON as peak.BTurnOff is now False") 
+                    
+        else: 
+            if zigbee_controller.get_device_state(device_id_b) == "OFF":
+                zigbee_controller.turn_on_device(device_id_b)"""
+
         values['c_turned_off'] = peak.CTurnOff
+
+        """ if not values['c_manueel']:
+            # Check if the device should be turned OFF
+            if peak.CTurnOff:
+                if zigbee_controller.get_device_state(device_id_c) == "ON":
+                    zigbee_controller.turn_off_device(device_id_c)
+                    print(f"{device_id_c} turned OFF due to peak.CTurnOff")
+
+            # Check if the device should be turned back ON
+            else:  # peak.CTurnOff is False
+                if zigbee_controller.get_device_state(device_id_c) == "OFF":
+                    zigbee_controller.turn_on_device(device_id_c)
+                    print(f"{device_id_c} turned ON as peak.CTurnOff is now False") 
+        else: 
+            if zigbee_controller.get_device_state(device_id_c) == "OFF":
+                zigbee_controller.turn_on_device(device_id_c)"""
 
 flask_th = Thread(target=flask_thread); flask_th.start()
 data_th = Thread(target=data_thread); data_th.start()
